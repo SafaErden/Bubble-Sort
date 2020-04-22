20741485d545868abf7c1b0c74f532dffbec996f
Kevin Alves <56762529+KevinAlvesJS@users.noreply.github.com>
2020-04-22 10:30:43 -0300

    check linter errors

--- bubblesort.rb
+++ bubblesort.rb
@@ -1,21 +1,13 @@
 def bubble_sort(arr)
   arr.each do
     i = 0
-    while i < arr.length-1 do
-     if arr[i]>arr[i+1]
-      arr[i], arr[i+1]=arr[i+1], arr[i]
-     end
-     i+=1
+    while i < arr.length - 1
+      arr[i], arr[i + 1] = arr[i + 1], arr[i] if arr[i] > arr[i + 1]
+      i += 1
     end
   end
   puts arr
 end
 
-bubble_sort([4,3,78,2,0,2,-2,-5])
-bubble_sort(['orange', 'tomato', 'apple', 'strawberry', 'coconut'])
-
-
-
-
-
-
+bubble_sort([4, 3, 78, 2, 0, 2, -2, -5])
+bubble_sort(%w[orange tomato apple strawberry coconut])
